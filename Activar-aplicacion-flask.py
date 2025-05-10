e49c3ed02c28ea4e59df6854ad48c84a5a292333
PiconAntonio <apicfer@g.educaand.es>
2025-05-10 19:59:36 +0200

    Create Activar-aplicacion-flask.py

---
+++ Activar-aplicacion-flask.py
@@ -0,0 +1,8 @@
+Activamos el entorno virtual : 
+source /opt/venv/bin/activate
+
+Luego nos vamos a ir al escritorio donde corre flask:
+cd /var/www/flaskapp
+
+Y ejecutamos el comamdo siguiente para desplegar la app: 
+python3 app.py
