6a8fdd3db7e1f76752e18144d18cf5c9a59f1803
PiconAntonio <apicfer@g.educaand.es>
2025-05-20 19:36:36 +0200

    Create creacion-carpetas.sh

---
+++ ansible/zabbix/creacion-carpetas.sh
@@ -0,0 +1,12 @@
+mkdir -p zbx_env/usr/share/zabbix/modules
+mkdir -p zbx_env/var/lib/zabbix/export
+mkdir -p zbx_env/var/lib/zabbix/modules
+mkdir -p zbx_env/var/lib/zabbix/enc
+mkdir -p zbx_env/var/lib/zabbix/ssh_keys
+mkdir -p zbx_env/var/lib/zabbix/mibs
+mkdir -p zbx_env/var/lib/zabbix/snmptraps
+mkdir -p zbx_env/usr/lib/zabbix/alertscripts
+mkdir -p zbx_env/usr/lib/zabbix/externalscripts
+mkdir -p zbx_env/etc/ssl/nginx
+mkdir -p zbx_env/etc/zabbix/zabbix_agentd.d
+mkdir -p zbx_env/var/lib/postgresql/data
